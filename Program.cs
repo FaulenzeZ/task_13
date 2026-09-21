d036a7e6c4cd9a741dd0d51fc1bb69e25cd10e55
FaulenzeZ <idrisov3688066@gmail.com>
2026-09-21 11:39:18 +0700

    save

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Zadacha25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            if (A == B)
+            {
+                Console.WriteLine("=");
+            }
+            else if (A > B)
+            {
+                Console.WriteLine(">");
+            }
+            else
+            {
+                Console.WriteLine("<");
+            }
+        }
+    }
+}
